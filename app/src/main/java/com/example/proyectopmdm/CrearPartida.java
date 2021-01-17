ab60beb0f7191d436fb238d92d549f9b82f03c4f
Lofly <gbo1karlosivan@gmail.com>
2021-01-17 19:28:29 +0100

    pantalla de crear partida

---
+++ app/src/main/java/com/example/proyectopmdm/CrearPartida.java
@@ -0,0 +1,14 @@
+package com.example.proyectopmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPartida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_partida);
+    }
+}
--- app/src/main/java/com/example/proyectopmdm/MainActivity.java
+++ app/src/main/java/com/example/proyectopmdm/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.proyectopmdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        boton = findViewById(R.id.Boton_Crear_partida);
 
-    public void prueba(){
+        //   boton.setOnClickListener(eventoClick);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent crear = new Intent(MainActivity.this, CrearPartida.class);
+                startActivity(crear);
+            }
+        });
+
 
 
         //https://code.tutsplus.com/es/tutorials/working-with-git-in-android-studio--cms-30514
